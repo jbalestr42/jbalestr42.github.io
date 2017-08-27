a37e49dac035551a517132303d8ed7b376090913
Balestrieri Julien <julien.balestrieri@gmail.com>
2017-08-27 17:34:11 +0200

    improve navbar resize

--- scrolling-nav.js
+++ scrolling-nav.js
@@ -12,4 +12,14 @@
 		offset: 54
 	});
 
+	$(document).on("scroll", function() {
+	
+		if($(document).scrollTop()>100) {
+			$("nav").removeClass("large").addClass("small");
+		} else {
+			$("nav").removeClass("small").addClass("large");
+		}
+		
+	});
+
 })(jQuery); // End of use strict
