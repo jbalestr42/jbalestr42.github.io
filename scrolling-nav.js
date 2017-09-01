e5a633bee0477ad7ec731139849197c4e8d54b20
Balestrieri Julien <julien.balestrieri@gmail.com>
2017-09-01 14:14:42 +0200

    contact form is working !

--- scrolling-nav.js
+++ scrolling-nav.js
@@ -18,6 +18,52 @@
 		} else {
 			$("nav").removeClass("small").addClass("large");
 		}
-		
+
 	});
 })(jQuery); // End of use strict
+
+var $contactForm = $('#contactForm');
+$contactForm.submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		url: '//formspree.io/julien.balestrieri@gmail.com',
+		method: 'POST',
+		data: $(this).serialize(),
+		dataType: 'json',
+		beforeSend: function() {
+			$contactForm.find('.alert-empty').hide();
+			$contactForm.find('.alert-info').show();
+		},
+		success: function(data) {
+			$contactForm.find('.alert-info').hide();
+			$contactForm.find('.alert-success').show();
+			$contactForm.find('.btn-send').attr("disabled", true);
+			setTimeout(clearSuccess, 5000);
+		},
+		error: function(err) {
+			$contactForm.find('.alert-info').hide();
+			$contactForm.find('.alert-danger').show();
+			setTimeout(clearError, 10);
+		}
+	});
+});
+
+function clearError()
+{
+	$contactForm.find('.alert-info').hide();
+	$contactForm.find('.alert-success').hide();
+	$contactForm.find('.alert-error').hide();
+	$contactForm.find('.alert-empty').show();
+	$contactForm.find('.btn-send').attr("disabled", false);
+}
+
+function clearSuccess()
+{
+	$contactForm.find('.alert-info').hide();
+	$contactForm.find('.alert-success').hide();
+	$contactForm.find('.alert-error').hide();
+	$contactForm.find('.alert-empty').show();
+	$contactForm.find('.form-message').val("");
+	$contactForm.find('.btn-send').attr("disabled", false);
+}
+
